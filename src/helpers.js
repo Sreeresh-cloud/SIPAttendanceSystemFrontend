24a4c2d259b3fd953c049088a77439060e988c0f
Sreeresh-cloud <sreereshss2@gmail.com>
2024-09-12 18:48:49 +0530

    latest commit

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,13 @@
 export const DAYS = [
-  "2024-09-09",
   "2024-09-10",
   "2024-09-11",
   "2024-09-12",
   "2024-09-13",
   "2024-09-14",
+  "2024-09-23",
+  "2024-09-24",
+
+
 ];
 
 export const dateFormatter = new Intl.DateTimeFormat("en-IN", {
